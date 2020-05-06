605e78beb20c5e0ec5bab0d53a2bd026a2f9fe83
Didrik Birkemalm <didrik.birkemalm@gmail.com>
2020-05-06 22:55:11 +0200

    Added db support

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const db = require('./db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var codesRouter = require('./routes/codes');
 
 const app = express();
 const port = process.env.PORT || "8000";
@@ -23,6 +25,7 @@ app.use('/static', express.static('public'))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/codes', codesRouter);
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
@@ -40,13 +43,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.error(err);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {err: res.locals.message, status: err.status});
 });
 
 module.exports = app;
